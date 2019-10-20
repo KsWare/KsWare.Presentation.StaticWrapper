7657bb5bf01fe0305f1aa6d3967790f0e1c07251
SchreinerK <ks@ksware.de>
2019-10-20 11:39:16 +0200

    Migrate to Core 3.0 + Multitarget netcoreapp3.0;net45

--- src/KsWare.Presentation.StaticWrapper/Properties/AssemblyInfo.cs
+++ src/KsWare.Presentation.StaticWrapper/Properties/AssemblyInfo.cs
@@ -1,19 +1,10 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+using System.Windows;
 
-[assembly: AssemblyTitle("KsWare.Presentation.StaticWrapper")]
-[assembly: AssemblyDescription("Wrapper for static .NET framework classes.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("KsWare")]
-[assembly: AssemblyProduct("KsWare.Presentation.StaticWrapper")]
-[assembly: AssemblyCopyright("Copyright © 2019 by KsWare. All rights reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-
-[assembly: ComVisible(false)]
-[assembly: Guid("36edb8ef-6238-49d4-880b-2cfe36629084")]
-
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0.0")]
+[assembly: ThemeInfo(
+	ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+									 //(used if a resource is not found in the page,
+									 // or application resource dictionaries)
+	ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+											  //(used if a resource is not found in the page,
+											  // app, or any theme specific resource dictionaries)
+)]
